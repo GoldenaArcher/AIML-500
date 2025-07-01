68acbca8e1ad0ffc02e17ac9cd5e5f3d9850c44e
GoldenaArcher <25376645+GoldenaArcher@users.noreply.github.com>
2025-07-01 07:36:15 -0400

    build(next): add assetPrefix for prod to support basePath
    Set assetPrefix conditionally based on NODE_ENV to ensure assets are correctly loaded in production when basePath is configured

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   basePath: '/AIML-500',
+  assetPrefix: isProd ? "/AIML-500/" : "",
   images: { unoptimized: true },
 };
 
